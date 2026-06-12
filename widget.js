bf1339357f1f9987997eea7a98ede8b4366911da
Pravin-digipro <pravin@digi-pro.co>
2026-06-12 17:46:16 +0530

    Add chatbot iframe to widget.js

---
+++ widget.js
@@ -0,0 +1,16 @@
+(function () {
+  const iframe = document.createElement("iframe");
+
+  iframe.src =
+    "https://grey-badger-770232.hostingersite.com/chatbot.html";
+
+  iframe.style.position = "fixed";
+  iframe.style.bottom = "20px";
+  iframe.style.right = "20px";
+  iframe.style.width = "420px";
+  iframe.style.height = "700px";
+  iframe.style.border = "none";
+  iframe.style.zIndex = "999999";
+
+  document.body.appendChild(iframe);
+})();
